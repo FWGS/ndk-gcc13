f0c854b40759ace17923d5ef242011a3234f9bcb
mittorn <mittorn@sibmail.com>
2023-11-06 04:41:49 +0300

    Initial upload

---
+++ toolchains/arm-linux-androideabi-13.2.1/prebuilt/linux-x86_64/lib/gcc/arm-linux-androideabi/13.2.1/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "../configure --prefix=/tmp/prefix/ --target=arm-linux-androideabi --host=x86_64-linux-gnu --build=x86_64-linux-gnu --with-gnu-as --with-gnu-ld --enable-languages=c,c++ --with-host-libstdcxx='-static-libgcc -Wl,-Bstatic,-lstdc++,-Bdynamic -lm' --disable-libssp --enable-threads --disable-nls --disable-libmudflap --disable-libstdc__-v3 --disable-sjlj-exceptions --disable-shared --disable-tls --disable-libitm --with-float=soft --with-fpu=vfp --with-arch=armv5te --enable-target-optspace --enable-initfini-array --disable-nls --with-sysroot=/tmp/sysroot/ --with-binutils-version=2.25 --with-bugurl=http://source.android.com/source/report-bugs.html --enable-languages=c,c++ --disable-bootstrap --enable-plugins --enable-libgomp --enable-gnu-indirect-function --enable-libsanitizer --enable-gold --enable-threads --enable-graphite=yes --enable-eh-frame-hdr-for-static --with-arch=armv5te --enable-gold=default --with-gmp=/tmp/prefix --with-mpfr=/tmp/prefix --with-mpc=/tmp/prefix --with-isl=/tmp/prefix/";
+static const char thread_model[] = "posix";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { "cpu", "arm10tdmi" }, { "arch", "armv5te" }, { "float", "soft" }, { "fpu", "vfp" }, { "tls", "gnu" } };
